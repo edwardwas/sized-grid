8a063b0431b237e484477f4cc102cb842f1cac6d
Ed Wastell <ed@wastell.co.uk>
2018-03-31 13:01:57 +0100

    Added neighbourhood point functoions

--- src/SizedGrid/Coord.hs
+++ src/SizedGrid/Coord.hs
@@ -131,3 +131,46 @@ coordPosition (Coord a) =
         helper (I c :* (cs :: NP I ys)) =
             ordinalToNum (c ^. asOrdinal) + ((maxCoordSize $ I c) * helper cs)
     in (helper a)
+
+type family AllDiffSame a xs :: Constraint where
+  AllDiffSame _ '[] = ()
+  AllDiffSame a (x ': xs) = (Diff x ~ a, AllDiffSame a xs)
+
+moorePoints ::
+     forall a cs. (Enum a, Num a, AllDiffSame a cs, All AffineSpace cs)
+  => a
+  -> Coord cs
+  -> [Coord cs]
+moorePoints n (Coord cs) =
+  let helper :: (All AffineSpace xs, AllDiffSame a xs) => NP I xs -> [NP I xs]
+      helper Nil = [Nil]
+      helper (I a :* as) = do
+        delta <- [-n .. n]
+        next <- helper as
+        return (I (a .+^ delta) :* next)
+  in map Coord $ helper cs
+
+vonNeumanPoints ::
+     forall a cs.
+     ( Enum a
+     , Num a
+     , Ord a
+     , All Integral (MapDiff cs)
+     , All AdditiveGroup (MapDiff cs)
+     , AllDiffSame a cs
+     , All AffineSpace cs
+     , Ord (CoordDiff cs)
+     , IsProductType (CoordDiff cs) (MapDiff cs)
+     , AdditiveGroup (CoordDiff cs)
+     )
+  => a
+  -> Coord cs
+  -> [Coord cs]
+vonNeumanPoints n c =
+  let helper :: Coord cs -> Bool
+      helper new =
+        sum
+          (hcollapse $
+           hcmap (Proxy :: Proxy Integral) (\(I a) -> K (abs $ fromIntegral a)) $ from (min (new .-. c) (c .-. new))) <=
+        n
+  in filter helper $ moorePoints n c
