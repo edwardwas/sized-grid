0ab5144ed92925cd42f98533b3a9e7afab6cea92
edwardwas <ed@wastell.co.uk>
2018-04-02 11:51:15 +0100

    Added many instances to Peano

--- src/SizedGrid/Peano.hs
+++ src/SizedGrid/Peano.hs
@@ -10,6 +10,8 @@
 
 module SizedGrid.Peano where
 
+import           Control.Lens
+import           Data.Maybe
 import           GHC.TypeLits
 
 data Peano = Z | S Peano
@@ -40,5 +42,54 @@ withSPeano :: SPeano n -> (SPeanoI n => x) -> x
 withSPeano SZ x     = x
 withSPeano (SS n) x = x
 
-class IsPositive (n :: Peano)
-instance (n ~ (S x), SPeanoI n) => IsPositive n
+demoteSPeano :: SPeano n -> Peano
+demoteSPeano SZ     = Z
+demoteSPeano (SS n) = S $ demoteSPeano n
+
+promoteSPeano :: Peano -> (forall n . SPeanoI n => SPeano n -> x) -> x
+promoteSPeano Z func     = func SZ
+promoteSPeano (S n) func = promoteSPeano n (\n' -> func (SS n'))
+
+peanoNegation :: Peano -> Peano -> Maybe Peano
+peanoNegation a Z         = Just a
+peanoNegation (S a) (S b) = peanoNegation a b
+peanoNegation _ _         = Nothing
+
+instance Num Peano where
+    S n + x = n + S x
+    Z + x = x
+    a - b = fromJust $ peanoNegation a b
+    x * Z = Z
+    Z * x = Z
+    a * S b = a + (a * b)
+    negate = error "Cannot negate Peano"
+    abs = id
+    signum _ = S Z
+    fromInteger x
+        | x < 0 = error ""
+        | x == 0 = Z
+        | x > 0 = S (fromInteger (x - 1))
+
+instance Real Peano where
+  toRational Z     = 0
+  toRational (S n) = 1 + toRational n
+
+instance Enum Peano where
+  succ = S
+  pred (S n) = n
+  pred Z     = error "Previous on Z"
+  fromEnum Z     = 0
+  fromEnum (S n) = 1 + fromEnum n
+  toEnum x
+    | x < 0 = error "To enum from negetaive peano"
+    | x == 0 = Z
+    | x > 0 = S (toEnum (x - 1))
+
+instance Integral Peano where
+    quotRem x y =
+        let helper n a = case peanoNegation a y of
+              Nothing -> (n, fromJust $ peanoNegation x (n * y))
+              Just c  -> helper (n + 1) c
+        in helper 0 x
+    toInteger Z     = 0
+    toInteger (S n) = 1 + toInteger n
