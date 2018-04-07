0631682cbd14ef1d40e52e66326d2af5779ee8b6
edwardwas <ed@wastell.co.uk>
2018-04-07 16:19:52 +0100

    Words

--- src/SizedGrid/Internal/Type.hs
+++ src/SizedGrid/Internal/Type.hs
@@ -6,6 +6,13 @@
 {-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE TypeOperators       #-}
 
+-- |
+-- Module      :  SizedGrid.Internal.Type
+-- Copyright   :  (C) 2018-18 Edward Wastell
+-- License     :  MIT -style (see the file LICENSE)
+-- Maintainer  :  Edward Wastell <ed@wastell.co.uk>
+-- Stability   :  provisional
+
 module SizedGrid.Internal.Type where
 
 import           Data.Constraint
@@ -13,12 +20,14 @@ import           Data.Proxy
 import           GHC.TypeLits
 import           Unsafe.Coerce
 
+-- | A singleton type for Bools
 data SBool a where
   STrue :: SBool 'True
   SFalse :: SBool 'False
 
 deriving instance Show (SBool a)
 
+-- | A type constraint for getting `SingI`
 class SBoolI a where
   sBool :: SBool a
 
@@ -28,6 +37,7 @@ instance SBoolI 'True where
 instance SBoolI 'False where
   sBool = SFalse
 
+-- | Give a runtime representation of a type level number being less than or equal than another
 sLessThan ::
        forall n m. (KnownNat n, KnownNat m)
     => Proxy n
@@ -38,11 +48,14 @@ sLessThan _ _ =
         then unsafeCoerce STrue
         else unsafeCoerce SFalse
 
+-- | A Dict prove that m - 1 + 1 is m
 takeAddIsId :: forall m . Dict (((m - 1) + 1) ~ m)
 takeAddIsId = unsafeCoerce (Dict :: Dict (a ~ a))
 
+-- | Magic is stole from Constraints, and I don't really understand it, but it is needed for 'takeNat'
 newtype Magic n = Magic (KnownNat n => Dict (KnownNat n))
 
+-- | Also don't understand
 magic ::
        forall n m o.
        (Integer -> Integer -> Integer)
@@ -53,5 +66,6 @@ magic f =
         (Magic Dict)
         (natVal (Proxy :: Proxy n) `f` natVal (Proxy :: Proxy m))
 
+-- | Runtime proof that n - m is an insance of KnownNat is n and m are
 takeNat :: (KnownNat n, KnownNat m) :- KnownNat (n - m)
 takeNat = magic (-)
